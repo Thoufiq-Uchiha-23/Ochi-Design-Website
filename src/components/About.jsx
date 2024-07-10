43ee4df525b3910d59f61ec19df34ff31df2fe4c
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-07-10 23:11:48 +0530

    id

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='w-full p-[50px] md:p-20 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl text-black'>
+    <div id="about" className='w-full p-[50px] md:p-20 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl text-black'>
         <h1 className="font-['Neue Montreal'] text-[7vw] md:text-[4vw] font-semibold tracking-tight leading-[7vw] md:leading-[4.5vw] ">
         Ochi is a strategic partner for fast-growing tech businesses that need to raise funds, sell products, explain complex ideas, and hire great people.
         </h1>
