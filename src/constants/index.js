b673259a3a1afa67863e09f8ddd1df88b400e9b4
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-07-10 23:10:59 +0530

    index js

---
+++ src/constants/index.js
@@ -0,0 +1,27 @@
+export const header = [
+    {
+        id: 0,
+        title: "Services",
+        url: "#services"
+    },
+    {
+        id: 1,
+        title: "Our Work",
+        url: "#work"
+    },
+    {
+        id: 2,
+        title: "About Us",
+        url: "#about"
+    },
+    {
+        id: 3,
+        title: "Insights",
+        url: "#insights"
+    },
+    {
+        id: 4,
+        title: "Contacts",
+        url: "#contact"
+    }
+]
